21d62ec58ae7736a4bfa88fa7d1484ede388f69b
xjubep <xjubep@gmail.com>
2022-09-13 08:36:24 +0000

    feat: add shell script

---
+++ run.sh
@@ -0,0 +1,3 @@
+python extract_simulated.py --model desc_1st --feature_path /hdd/sy/VCDB_simulated/features/test/desc_1st;
+python extract_simulated.py --model hybrid_vit --feature_path /hdd/sy/VCDB_simulated/features/test/hybrid_vit;
+python extract_simulated.py --model mobilenet_avg --feature_path /hdd/sy/VCDB_simulated/features/test/mobilenet_avg;
